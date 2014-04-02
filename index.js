fa6f7a5a21e522caef87115a327acbc9cbda1c39
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-04-02 23:11:30 +0800

    Change story with sensor

--- index.js
+++ index.js
@@ -10,4 +10,38 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
   socket.emit('begin', {text: 'story begin'});
+
+  // ugly hacking to emit event from socket by johnny-five
+  var five = require('johnny-five');
+  var board = new five.Board();
+
+  board.on('ready', function () {
+    sensor = new five.Sensor({
+      pin: 'A0',
+      freq: 250
+    });
+
+    var last;
+    var threshold = -20;
+    sensor.scale([0, 100]).on('data', function () {
+      console.log(this.value, this.raw);
+      if (!last) {
+        last = this.value;
+      } else {
+        var story = this.value - last;
+        last = this.value;
+        if (story < threshold) {
+          console.log(story);
+          socket.emit('story', {text: story});
+        }
+      }
+    });
+
+    board.repl.inject({
+      sensor: sensor,
+      threshold: threshold
+    });
+
+  });
+
 });
